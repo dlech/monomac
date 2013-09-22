1acc1664718701a60619ddb8cd7b3dbfa8145906
David Lechner <david@lechnology.com>
2013-09-22 16:04:57 -0500

    Add bindings for IOKit for accessing USB.

--- src/Constants.cs
+++ src/Constants.cs
@@ -56,5 +56,6 @@ namespace MonoMac {
 		public const string CoreMidiLibrary = "/System/Library/Frameworks/CoreMIDI.framework/CoreMIDI";
 		public const string QuickLookLibrary = "/System/Library/Frameworks/QuickLook.framework/QuickLook";
 		public const string AVFoundationLibrary = "/System/Library/Frameworks/AVFoundation.framework/AVFoundation";
-        }
+		public const string IOKitLibrary = "/System/Library/Frameworks/IOKit.framework/IOKit";
+	}
 }
